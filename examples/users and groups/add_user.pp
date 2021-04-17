64fb52c452a2e71fe1193c7554e3513bfb914e6e
kinners00 <marcmckinley@me.com>
2021-04-17 13:59:50 +0100

    push

---
+++ examples/users and groups/add_user.pp
@@ -0,0 +1,24 @@
+  # basic example
+  user { 'Joe Bloggs':
+    ensure   => 'present',
+    password => 'mypassword123',
+  }
+
+  # groups example
+  user { 'Joe Bloggs':
+    ensure   => present,
+    password => 'mypassword123',
+    groups   => ['Administrators'],
+  }
+
+
+  # expanded nix example
+  user { 'Joe Bloggs':
+    ensure   => 'present',
+    comment  => 'SE Demo Account',
+    gid      => '100',
+    home     => '/',
+    password => 'mypassword123',
+    shell    => '/bin/bash',
+    uid      => '1010',
+  }
