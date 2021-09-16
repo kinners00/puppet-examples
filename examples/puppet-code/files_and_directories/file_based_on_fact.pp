6a9bea5b408837ee8207a8edf223da56ee79955c
Marc McKinley <marc@puppet.com>
2021-09-16 16:56:43 +0100

    Update file_based_on_fact.pp

--- examples/puppet-code/files_and_directories/file_based_on_fact.pp
+++ examples/puppet-code/files_and_directories/file_based_on_fact.pp
@@ -1,8 +1,4 @@
   file { '/etc/mysql/conf.d/tuning.cnf':
     ensure => 'present',
-    source => [
-        "puppet:///modules/mysql-server/tuning.${::hostname.cnf}",
-        "puppet:///modules/mysql-server/tuning.${::domain.cnf}",
-        'puppet:///modules/mysql-server/tuning.cnf'
-    ],
+    source => "puppet:///modules/mysql-server/tuning.${::hostname.cnf}",
   }
