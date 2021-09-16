1ab616665d4f261a176c61bbbcedaff7841773ba
Marc McKinley <marc@puppet.com>
2021-09-16 16:57:28 +0100

    Update template_based_on_fact.pp

--- examples/puppet-code/files_and_directories/template_based_on_fact.pp
+++ examples/puppet-code/files_and_directories/template_based_on_fact.pp
@@ -2,9 +2,5 @@
   file { '/etc/ssh/sshd_config' :
     ensure  => present,
     mode    => '0600',
-    content => multitemplate(
-                              "ssh/${::fqdn}.erb",
-                              "ssh/${::domain}.erb",
-                              'ssh/default_sshdconfig.erb'
-                            ),
+    content => "ssh/${::fqdn}.erb",
   }
