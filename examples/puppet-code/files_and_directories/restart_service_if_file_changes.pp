d42a546fa4aadf614ef4464f7491278ed3979262
Marc McKinley <marc@puppet.com>
2021-09-16 16:52:20 +0100

    Update restart_service_if_file_changes.pp

--- examples/puppet-code/files_and_directories/restart_service_if_file_changes.pp
+++ examples/puppet-code/files_and_directories/restart_service_if_file_changes.pp
@@ -1,16 +1,31 @@
-  # CHANGE example
+  # The two examples below create the same relationship
+  
+  
+  # add subscribe to the service resource
   service { 'sshd':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['openssh-server'],
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/ssh/sshd_config'],
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure => file,
+    mode   => '0600',
+    source => 'puppet:///modules/sshd/sshd_config',
+}
+
+
+  # or
+  
+  service { 'sshd':
+    ensure    => running,
+    enable    => true,
   }
 
   # add a notify to the file resource
   file { '/etc/ssh/sshd_config':
-    notify  => Service['sshd'],  # this sets up the relationship
-    mode    => '0600',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['openssh-server'],
-    content => template('ssh/sshd_config.erb'),
-  }
+    ensure => file,
+    mode   => '0600',
+    source => 'puppet:///modules/sshd/sshd_config',
+    notify => Service['sshd'],
+}
